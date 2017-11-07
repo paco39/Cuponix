036ff8c81d676ed9af06fca5d78fc46a913bd426
paco39 <pako03.fm@gmail.com>
2017-11-07 03:30:31 -0600

    Initial commit

---
+++ app/src/main/java/mx/uv/cuponix/HitorialCuponesActivity.java
@@ -0,0 +1,13 @@
+package mx.uv.cuponix;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HitorialCuponesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hitorial_cupones);
+    }
+}
